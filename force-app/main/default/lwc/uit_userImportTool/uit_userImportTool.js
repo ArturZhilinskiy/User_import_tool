84c3e5a3db4b69066f4219e74d170dc10313f135
@artur.zhilinskiy <artur.zhilinskiy@vrpconsulting.com>
2020-07-28 12:52:41 +0300

    Fix

--- force-app/main/default/lwc/uit_userImportTool/uit_userImportTool.js
+++ force-app/main/default/lwc/uit_userImportTool/uit_userImportTool.js
@@ -6,46 +6,33 @@ import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled } from 'lig
 
 export default class Uit_userImportTool extends NavigationMixin(LightningElement) {
     @track channelName = '/event/UIT_Import_Event__e';
-    @track isDataRecived = false;
+    @track isDataReceived = false;
     @track isLoading = false;
     @track importResult = {
         importedRecords : 0,
         updatedRecords : 0,
         errors : 0,
-        importStatus : 'Sucess'
+        importStatus : 'Success'
     }
 
     subscription = {};
    
     connectedCallback() {
-        let resultObj = {};
-        const messageCallback = function(response) { 
-            
-           
-            console.log('New message received : ', JSON.stringify(response));
-            
-            resultObj.errors = response.data.payload.errors__c;
-            console.log('haha2');
-            resultObj.updatedRecords = response.data.payload.updatedRecords__c;
-            resultObj.importedRecords = response.data.payload.importedRecords__c;
-            resultObj.importStatus = response.data.payload.importStatus__c;
-            console.log('resultObj', resultObj);
-            this.importResult = resultObj;
-            console.log('importResult', importResult);
-            
-        };
-
-        subscribe(this.channelName, -1, messageCallback).then(response => {
+        subscribe(this.channelName, -1, (response) => {
+            this.importResult = {
+                importedRecords : response.data.payload.importedRecords__c,
+                updatedRecords : response.data.payload.updatedRecords__c,
+                errors : response.data.payload.errors__c,
+                importStatus : response.data.payload.importStatus__c
+            };
+            this.isLoading = false;
+            this.isDataReceived = true;
+        }).then(response => {
             console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
             this.subscription = response;
         });
 
         
-    }
-
-    handleMessageCallback(response) {
-        this.isLoading = false;
-        this.isDataRecived = true;
     }
 
     handleImportData() {
@@ -64,7 +51,7 @@ export default class Uit_userImportTool extends NavigationMixin(LightningElement
             };
             console.log(error);
             this.isLoading = false;
-            this.isDataRecived = true;
+            this.isDataReceived = true;
         });
 
         
