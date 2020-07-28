44e07c551403b87bc0e3da9ca46a2c7769099bd2
@artur.zhilinskiy <artur.zhilinskiy@vrpconsulting.com>
2020-07-28 12:39:42 +0300

    last changes

--- force-app/main/default/lwc/uit_userImportTool/uit_userImportTool.js
+++ force-app/main/default/lwc/uit_userImportTool/uit_userImportTool.js
@@ -1,38 +1,73 @@
 import { LightningElement, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import importUsers from '@salesforce/apex/UserImportServiceClass.importUsers';
+import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled } from 'lightning/empApi';
 
 
 export default class Uit_userImportTool extends NavigationMixin(LightningElement) {
+    @track channelName = '/event/UIT_Import_Event__e';
     @track isDataRecived = false;
     @track isLoading = false;
     @track importResult = {
-        importedRecords : 1,
-        updatedRecords : 13,
+        importedRecords : 0,
+        updatedRecords : 0,
+        errors : 0,
         importStatus : 'Sucess'
     }
+
+    subscription = {};
    
+    connectedCallback() {
+        let resultObj = {};
+        const messageCallback = function(response) { 
+            
+           
+            console.log('New message received : ', JSON.stringify(response));
+            
+            resultObj.errors = response.data.payload.errors__c;
+            console.log('haha2');
+            resultObj.updatedRecords = response.data.payload.updatedRecords__c;
+            resultObj.importedRecords = response.data.payload.importedRecords__c;
+            resultObj.importStatus = response.data.payload.importStatus__c;
+            console.log('resultObj', resultObj);
+            this.importResult = resultObj;
+            console.log('importResult', importResult);
+            
+        };
+
+        subscribe(this.channelName, -1, messageCallback).then(response => {
+            console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
+            this.subscription = response;
+        });
+
+        
+    }
+
+    handleMessageCallback(response) {
+        this.isLoading = false;
+        this.isDataRecived = true;
+    }
 
     handleImportData() {
         this.isLoading = true;
 
-        
         importUsers()
-            .then(result => {
-                this.importResult = result;
-                this.isLoading=false;
-                this.isDataRecived = true;
-            })
-            .catch(error => {
-                this.importResult = {
-                    importedRecords : 0,
-                    updatedRecords : 0,
-                    importStatus : 'Error: ' + error.body.message + ' ' + error.body.stackTrace
-                };
-                console.log(error);
-                this.isLoading=false;
-                this.isDataRecived = true;
-            });
+        .then(result => {
+            
+        })
+        .catch(error => {
+            this.importResult = {
+                'importedRecords' : 0,
+                'updatedRecords' : 0,
+                'errors' : 0,
+                'importStatus' : 'Error: ' + error.body.message + ' ' + error.body.stackTrace
+            };
+            console.log(error);
+            this.isLoading = false;
+            this.isDataRecived = true;
+        });
+
+        
     }
 
     handleViewRecords() {
